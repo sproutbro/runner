1436abbcb3216ff6c5d5941e2728a016de0103fe
Jackie <krsproutbro@google.com>
2025-04-05 02:14:34 +0900

    ddd

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-import 'dotenv/config';
 import express from "express";
-import cookieParser from 'cookie-parser';
 
 const app = express();
 app.use(express.static('public'));
 app.use(express.json());
-app.use(cookieParser());
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3006;
 app.listen(PORT, console.log(`http://localhost:${PORT}`));
