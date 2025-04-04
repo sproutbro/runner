14020bb6aa98820eaffff99c577cf7bf7c437a3b
Jackie <krsproutbro@google.com>
2025-04-04 10:23:24 +0900

    몰라

--- server/db/score.js
+++
@@ -1,8 +0,0 @@
-import { db } from './index.js';
-
-export async function saveScore({ userId, game, score }) {
-    await db.query(
-        'INSERT INTO scores (user_id, game, score) VALUES ($1, $2, $3)',
-        [userId, game, score]
-    );
-}
